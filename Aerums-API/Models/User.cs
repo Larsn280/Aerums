e6b9aeb257605ad9de6801015559b4f46174d8a8
Marielle Lunnan <marielle.lunnan@learnet.se>
2023-03-20 23:17:19 +0100

    Models

--- Aerums-API/Models/User.cs
+++ Aerums-API/Models/User.cs
@@ -1,6 +1,12 @@
-﻿namespace Aerums_API.Models
+﻿using Microsoft.AspNetCore.Identity;
+namespace Aerums_API.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<FreeTimeModel>? FreeTimes { get; set; }
+        public List<BookingModel>? Bookings { get; set; }
+        public List<User>? MyFriends { get; set; }
     }
 }
