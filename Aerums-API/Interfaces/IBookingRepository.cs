9b9594192e26ade9e31cf277256326e7eb97587e
Dennis Lindquist <dennislindquist@gmail.com>
2023-04-04 21:53:11 +0200

    ok läz

--- Aerums-API/Interfaces/IBookingRepository.cs
+++ Aerums-API/Interfaces/IBookingRepository.cs
@@ -1,12 +1,13 @@
-
+using Aerums_API.Models;
 using Aerums_API.ViewModels.BookingViewModels;
 
-namespace Aerums_API.Interfaces
-{
-    public interface IBookingRepository
-    {
-        public Task<List<BookingViewModel>> ListAllBookingsAsync();
+namespace Aerums_API.Interfaces {
+    public interface IBookingRepository {
+        public Task<List<BookingViewModel>> ListAllBookingsAsync ();
 
-        public Task<BookingViewModel> GetBookingByIdAsync(int id);
+        public Task<BookingViewModel> GetBookingByIdAsync (int id);
+
+        public Task<BookingModel> DeleteBooking (int id);
+
     }
 }
--- Aerums-API/Repositories/BookingRepository.cs
+++ Aerums-API/Repositories/BookingRepository.cs
@@ -4,25 +4,20 @@ using Aerums_API.Models;
 using Aerums_API.ViewModels.BookingViewModels;
 using Microsoft.EntityFrameworkCore;
 
-namespace Aerums_API.Repositories
-{
-    public class BookingRepository : IBookingRepository
-    {
+namespace Aerums_API.Repositories {
+    public class BookingRepository : IBookingRepository {
         private readonly AerumsContext _context;
 
-        public BookingRepository(AerumsContext context)
-        {
+        public BookingRepository (AerumsContext context) {
             _context = context;
         }
 
-        public async Task<BookingViewModel> GetBookingByIdAsync(int id)
-        {
-            var selectedBooking = await _context.BookingModel!.FindAsync(id);
+        public async Task<BookingViewModel> GetBookingByIdAsync (int id) {
+            var selectedBooking = await _context.BookingModel!.FindAsync (id);
 
-            BookingViewModel booking = new BookingViewModel();
+            BookingViewModel booking = new BookingViewModel ();
 
-            if (selectedBooking != null)
-            {
+            if (selectedBooking != null) {
                 booking.Date = selectedBooking.Date;
                 booking.StartTime = selectedBooking.StartTime;
                 booking.EndTime = selectedBooking.EndTime;
@@ -35,17 +30,14 @@ namespace Aerums_API.Repositories
             return null!;
         }
 
-        public async Task<List<BookingViewModel>> ListAllBookingsAsync()
-        {
-            List<BookingModel> allBookings = await _context.BookingModel!.ToListAsync();
-            List<BookingViewModel> booked = new List<BookingViewModel>();
-            BookingViewModel newBooking = new BookingViewModel();
+        public async Task<List<BookingViewModel>> ListAllBookingsAsync () {
+            List<BookingModel> allBookings = await _context.BookingModel!.ToListAsync ();
+            List<BookingViewModel> booked = new List<BookingViewModel> ();
+            BookingViewModel newBooking = new BookingViewModel ();
 
-            foreach (var booking in allBookings)
-            {
+            foreach (var booking in allBookings) {
 
-                newBooking = new BookingViewModel
-                {
+                newBooking = new BookingViewModel {
                     Date = booking.Date,
                     StartTime = booking.StartTime,
                     EndTime = booking.EndTime,
@@ -54,10 +46,16 @@ namespace Aerums_API.Repositories
                     Note = booking.Note
                 };
 
-                booked.Add(newBooking);
+                booked.Add (newBooking);
             }
 
             return booked;
+        }
+
+        public async Task<BookingModel> DeleteBooking (int id) {
+            var selectedBooking = await _context.BookingModel!.FindAsync (id);
+            _context.BookingModel.Remove (selectedBooking!);
+            return selectedBooking!;
         }
     }
 }
